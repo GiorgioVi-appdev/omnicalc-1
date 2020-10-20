07e3758fe4a526682e109f04df260d14375e8682
GiorgioVi <gvidali@uchicago.edu>
2020-10-20 05:32:27 +0000

    this is so long

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,27 @@
 class ApplicationController < ActionController::Base
 
+    def blank_square_form
+      render({:template=> "calculation_templates/square_form.html.erb"})
+    end
+    def blank_squareroot_form
+      render({:template=> "calculation_templates/squareroot_form.html.erb"})
+    end
+    def blank_payment_form
+      render({:template=> "calculation_templates/payment_form.html.erb"})
+    end
+    def blank_random_form
+      render({:template=> "calculation_templates/random_form.html.erb"})
+    end
+    def result_square_form
+      render({:template=> "calculation_templates/result_square_form.html.erb"})
+    end
+    def result_squareroot_form
+      render({:template=> "calculation_templates/result_squareroot_form.html.erb"})
+    end
+    def result_payment_form
+      render({:template=> "calculation_templates/result_payment_form.html.erb"})
+    end
+    def result_random_form
+      render({:template=> "calculation_templates/result_random_form.html.erb"})
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
 
+  get("/square/new",{:controller => "application", :action => "blank_square_form" })
+  get("/square_root/new",{:controller => "application", :action => "blank_squareroot_form" })
+  get("/payment/new",{:controller => "application", :action => "blank_payment_form" })
+  get("/random/new",{:controller => "application", :action => "blank_random_form" })
+  get("/square/results",{:controller => "application", :action => "result_square_form" })
+  get("/square_root/results",{:controller => "application", :action => "result_squareroot_form" })
+  get("/payment/results",{:controller => "application", :action => "result_payment_form" })
+  get("/random/results",{:controller => "application", :action => "result_random_form" })
+
 end
